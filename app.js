4f150552190a53b7142609d41cbc35526ec42655
Abhilasha Jairath <53110403+Abhilasha-Jairath@users.noreply.github.com>
2022-12-18 22:16:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,15 +23,14 @@ app.use(express.json());
 app.use(require("./router/auth"));
 const User = require("./model/userSchema");
 
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function(_, res) {
-    res.sendFile(
-        path.join(__dirname, "./client/build/public/index.html"),
-        function(err) {
-            res.status(500).send(err);
-        }
+if (process.env.NODE_ENV === "production") {
+    //*Set static folder up in production
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     );
-});
+}
 
 app.listen(5000, () => {
     console.log("server running on port number 3000");
